2cf3c0e0a8d8eaf7fda8a5192a700dcd37ba7f4f
joaojorge <joaojorgedosanjos@gmail.com>
2023-07-01 17:26:22 -0300

    Implementação de Listas Circulares(getNo() e get())

--- src/main/java/one/digitalinnovation/ListaCircular.java
+++ src/main/java/one/digitalinnovation/ListaCircular.java
@@ -3,8 +3,28 @@ package one.digitalinnovation;
 public class ListaCircular<T> {
 
     private No<T> cabeça;
-    private No<T> calda;
+    private No<T> cauda;
     private int tamanhoLista;
+
+    public T get(int index){
+        return this.getNo(index).getConteudo();
+    }
+
+    private No<T> getNo(int index) {
+        if (this.isEmpty())
+            throw new IndexOutOfBoundsException("A lista está vazia!");
+
+            if (index == 0) {
+                return this.cauda;
+            }
+
+            No<T> noAuxiliar = this.cauda;
+            for (int i = 0; (i < index) && (noAuxiliar != null); i++) {
+                noAuxiliar = noAuxiliar.getNoProximo();
+            }
+            return noAuxiliar;
+        }
+
 
     public boolean isEmpty() {
         return this.tamanhoLista == 0 ? true : false;
